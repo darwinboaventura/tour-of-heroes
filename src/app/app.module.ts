e8d41a886f46697a3b87147f16b8e7d227affe8e
Darwin Boaventura <darwinaraujo@ext.cvc.com.br>
2017-07-28 14:10:48 -0300

    parei https://angular.io/tutorial/toh-pt5#get-heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+// Services
+import { HeroService } from './hero.service';
+
+// My Components
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		HeroDetailComponent
+        HeroDetailComponent,
+		HeroesComponent,
+        DashboardComponent
 	],
 	imports: [
 		BrowserModule,
-		FormsModule
+		FormsModule,
+        RouterModule.forRoot([
+            {
+                path: 'heroes',
+                component: HeroesComponent
+            },
+            {
+                path: 'dashboard',
+                component: DashboardComponent
+            },
+            {
+                path: '',
+                redirectTo: '/dashboard',
+                pathMatch: 'full'
+            }
+        ])
 	],
-	providers: [],
+	providers: [HeroService],
 	bootstrap: [AppComponent]
 })
 
